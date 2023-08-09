c90ec7d30221cc28bcc20a670b56584d75b9de41
cruwmorgan <morganokeke@gmail.com>
2023-08-09 08:24:03 -0700

    coroutine will loop 10 times

---
+++ 0x02-python_async_comprehension/2-main.py
@@ -0,0 +1,15 @@
+bob@dylan:~$ cat 2-main.py
+#!/usr/bin/env python3
+
+import asyncio
+
+
+measure_runtime = __import__('2-measure_runtime').measure_runtime
+
+
+async def main():
+    return await(measure_runtime())
+
+print(
+    asyncio.run(main())
+)
